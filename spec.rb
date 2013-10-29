4720936d4c18a124c8dc9bc09baadb59e88b65ad
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:04:26 -0500

    changed specs for initialize

--- spec.rb
+++ spec.rb
@@ -1,18 +1,26 @@
 describe Pizza do
 
-  let(:pizza) { Pizza.new("Ham", "Awesomeness on a pizza", 4) }
+  let(:pizza) { Pizza.new("Ham", "Awesomeness on a pizza") }
 
   describe '#initialize' do
     it 'should error with invalid arguments' do
       expect { Pizza.new("yolo") }.to raise_error(ArgumentError)
     end
 
-    it 'has readable arguments' do
+    it 'has title' do
       pizza.title.should eq "Ham"
-      pizza.description.should eq "Awesomeness on a pizza"
-      pizza.time_baked.should eq 4
     end
-    
+
+    it 'has description' do
+      pizza.description.should eq "Awesomeness on a pizza"
+    end
+
+    it 'defaults time_baked to 0' do
+      pizza.time_baked.should eq 0
+    end
+
+    end
+
   end
 
 end
