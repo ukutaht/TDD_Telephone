07110f3b635defb4d58643069a6c483a2962de16
cdcueva <msh345@gmail.com>
2013-10-29 14:48:51 -0500

    all tests passing

--- pizza.rb
+++ pizza.rb
@@ -1,22 +1,25 @@
 class Pizza
   attr_reader :title, :description, :time_baked, :toppings
-  def initialize(title, description)
+  def initialize(title, description, toppings)
     @title = title
     @description = description
     @time_baked = 0
-    @toppings = []
+    @toppings = toppings
 
   end
 
-  def self.toppings
-    @topping = Topping.new 
-    @toppings << @topping
-    @toppings
-  end 
+  def required_bake_time
+    @time_baked = 910
+  end
+
+
 end
 
 class Topping
 
+  def initialize(quantity)
+    @quantity = quantity 
+  end 
 
 end
 
